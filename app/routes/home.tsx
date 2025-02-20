3d764560cb5fc6708ebb03bf47305d1fbe72c0b9
Igwe Reinhard <108090171+Rigwe19@users.noreply.github.com>
2025-02-20 18:47:57 +0100

    fixed description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import { Welcome } from "../welcome/welcome";
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Attih Soul" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Attih Soul is a Nigerian-born singer, songwriter, vocal coach, and acoustic guitarist whose captivating voice and soulful melodies have earned him recognition across continents. Currently based in the vibrant city of Barcelona, Spain, Attih’s artistry transcends cultural and musical boundaries, blending African roots with universal sounds to create timeless music." },
   ];
 }
 
