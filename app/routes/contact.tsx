321ed40ca28b72550c3bfa8a596c28b56ed336f5
Igwe Reinhard <108090171+Rigwe19@users.noreply.github.com>
2025-03-08 05:42:54 +0100

    testing contact form

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -4,6 +4,8 @@ import Footer from "~/components/footer";
 import Nav from "~/components/nav";
 import type { Route } from "./+types/contact";
 import Social from "~/components/social";
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import { useState } from "react";
 
 export function meta({ }: Route.MetaArgs) {
     return [
@@ -11,7 +13,20 @@ export function meta({ }: Route.MetaArgs) {
         { name: "description", content: "Welcome to React Router!" },
     ];
 }
+const postURL = `https://attihsoul.us7.list-manage.com/subscribe/post?u=0bd9ba2118b0697092bfa5182&id=48090a1e39`
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        MERGE2: '',
+        MERGE1: '',
+        EMAIL: ''
+    });
+    const handleChange = (value:string, name:string) => {
+      setFormData({
+        ...formData,
+        [name]: value
+      });
+    }
+    
     return (
         <div className="bg-white text-black">
             <div className="h-[774px]" style={{
@@ -25,25 +40,30 @@ const Contact = () => {
                     <h2 className="uppercase font-bold md:text-8xl text-4xl md:leading-28 tracking-[2px] md:tracking-[4.58px] text-center">contact</h2>
                 </div>
                 <div className="flex flex-col md:gap-20 gap-12">
-                    <div className="flex gap-11 flex-col-reverse md:flex-row">
-                        <div className="flex flex-col gap-4 w-full max-w-xl">
-                            <div className="flex flex-col gap-2 md:gap-6">
-                                <label htmlFor="name" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Name</label>
-                                <input type="text" className="border border-black py-2 focus-visible:outline-0" />
-                            </div>
-                            <div className="flex flex-col gap-2 md:gap-6">
-                                <label htmlFor="email" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Email</label>
-                                <input type="email" className="border border-black py-2 focus-visible:outline-0" />
-                            </div>
-                            <div className="flex flex-col gap-2 md:gap-6">
-                                <label htmlFor="message" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Message</label>
-                                <textarea rows={12} className="border border-black py-2 focus-visible:outline-0" />
-                            </div>
+                    <MailchimpSubscribe url={postURL} render={({ subscribe, status, message }) => (
+                        <>
+                            <div className="flex gap-11 flex-col-reverse md:flex-row">
+                                <div className="flex flex-col gap-4 w-full max-w-xl">
+                                    <div className="flex flex-col gap-2 md:gap-6">
+                                        <label htmlFor="name" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Name</label>
+                                        <input type="text" value={formData.MERGE1} onChange={e=>handleChange(e.target.value, 'MERGE1')} className="border border-black py-2 focus-visible:outline-0" />
+                                    </div>
+                                    <div className="flex flex-col gap-2 md:gap-6">
+                                        <label htmlFor="email" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Email</label>
+                                        <input type="email" value={formData.EMAIL} onChange={e=>handleChange(e.target.value, 'EMAIL')} className="border border-black py-2 focus-visible:outline-0" />
+                                    </div>
+                                    <div className="flex flex-col gap-2 md:gap-6">
+                                        <label htmlFor="message" className="md:text-xl leading-6 tracking-[2px] tracking-[4.58px]">Message</label>
+                                        <textarea value={formData.MERGE2} onChange={e=>handleChange(e.target.value, 'MERGE2')} rows={12} className="border border-black py-2 focus-visible:outline-0" />
+                                    </div>
 
-                        </div>
-                        <img src="/images/contact.png" alt="" />
-                    </div>
-                    <Button className="border border-black text-black px-12 w-fit self-center -mt-8">Send a Message</Button>
+                                </div>
+                                <img src="/images/contact.png" alt="" />
+                            </div>
+                            <Button onClick={()=>subscribe(formData)} className="border border-black text-black px-12 w-fit self-center -mt-8">Send a Message</Button>
+                        </>
+                    )} />
+
                 </div>
             </div>
             <Booking className="bg-white text-black" buttonClassName="border-black text-black" />
